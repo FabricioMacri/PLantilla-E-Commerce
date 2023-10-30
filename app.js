115170724eeac205d94978a0b51218d17a788b35
FabricioMacri <89487500+FabricioMacri@users.noreply.github.com>
2023-10-30 01:08:46 -0300

    Tarjetas modelo de productos

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
+/*
+                    Cada uno de estos por tarjeta y un row por cada fila
+                    
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Tarjeta 1</h5>
+                                <p class="card-text">Contenido de la tarjeta 1.</p>
+                            </div>
+                        </div>
+                    </div>
+*/
+
+function makeItem () {
+
+    const div = document.createElement("div");
+    
+}
+
+
 
 fetch('http://localhost:9000/api', {mode: 'cors'})
     .then(function(response) {
-    return response.text();
+    return response.json();
     })
-    .then(function(text) {
-    console.log('Request successful', text);
+    .then(function(data) {
+        
+        console.log(data);
+
+
     })
     .catch(function(error) {
-    console.log('Request failed', error)
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Parece que hubo un problema, intente mas tarde.'
+          })
     });
+
