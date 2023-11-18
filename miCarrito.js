4f86963959bafe469d44d50c81df29db65f219ac
fabriciomacri <fabriciomacri@hotmail.com>
2023-11-18 19:22:23 -0300

    comit colgado

--- miCarrito.js
+++ miCarrito.js
@@ -1,4 +1,12 @@
 
+/*
+<li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+        <div class="fw-bold">HP Omen</div>
+    </div>
+    <span class="badge bg-primary rounded-pill">2</span>
+</li>
+*/
 
 const buscadorPrincipal = document.getElementById("buscador");
 
