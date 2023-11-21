9936c86893f5a4d8c79bc82c8304ffe227cc912f
FabricioMacri <fabriciomacri@hotmail.com>
2023-11-21 00:50:18 -0300

    todo andando menos parasela de pago

--- miCarrito.js
+++ miCarrito.js
@@ -1,12 +1,124 @@
 
 /*
-<li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-        <div class="fw-bold">HP Omen</div>
+<div class="col-md-4">
+    <div class="card">
+        <div class="row">
+            <div class="col-md-6">
+                <img class="carritoImgItem" src="./image/pc.jpg" alt="">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">HP Omen 15</h5>
+                    <p class="card-text">$250000</p>
+                    <button class="btn btn-danger">Eliminar</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <span class="badge bg-primary rounded-pill">2</span>
-</li>
+</div>
 */
+function makeCarritoItem(item) {
+
+    const carrito = document.getElementById("carritoContainer");
+
+    const col4 = document.createElement("div");
+    const card = document.createElement("div");
+    const row = document.createElement("div");
+    const col6 = document.createElement("div");
+    const col62 = document.createElement("div");
+    const body = document.createElement("div");
+
+    const img = document.createElement("img");
+    const tt = document.createElement("h5");
+    const desc = document.createElement("p");
+    const btn = document.createElement("button");
+
+    col4.classList.add("col-md-4");
+    card.classList.add("card");
+    row.classList.add("row");
+    col6.classList.add("col-md-6");
+    col62.classList.add("col-md-6");
+    body.classList.add("card-body");
+
+    img.classList.add("carritoImgItem");
+    tt.classList.add("card-title");
+    img.classList.add("card-text");
+    btn.classList.add("btn");
+    btn.classList.add("btn-danger");
+
+    col4.id = "cont" + item.ID;
+    
+    tt.textContent = item.cantidad + "x " + item.name;
+    tt.id = "tt" + item.ID;
+    desc.textContent = "$" + item.price;
+    btn.textContent = "Eliminar";
+    btn.id = "btn" + item.ID;
+
+    btn.addEventListener("click", () => {
+
+        let carrito = JSON.parse(localStorage.getItem("carrito"));
+
+        let lista = [];
+
+        let total = 0;
+
+        if (carrito != undefined){
+
+            carrito.forEach(element => {
+
+                lista.push(element);
+                
+                total = total + (element.price * element.cantidad);
+            });
+        }
+
+        let producto = carrito.find((el) => ("btn" + el.ID) == btn.id);
+
+        producto.cantidad = producto.cantidad - 1;
+
+        if(producto.cantidad == 0) {
+
+            const carrito = document.getElementById("carritoContainer");
+            const container = document.getElementById("cont" + producto.ID);
+
+            carrito.removeChild(container);
+
+            console.log(lista.indexOf(producto));
+
+            let index = lista.indexOf(producto);
+
+            lista.splice(index, 1);
+            console.log(lista);
+        }
+        else {
+            const nombre = document.getElementById("tt" + producto.ID);
+            console.log(producto.ID);
+            console.log(nombre);
+            nombre.textContent = producto.cantidad + "x " + producto.name;  
+        }      
+
+        total = total - producto.price;
+        const textTotal = document.getElementById("totalCarrito");
+        textTotal.textContent = "Total: $" + total;
+
+        localStorage.setItem("carrito", JSON.stringify(lista));
+    })
+
+    img.setAttribute("src", item.img);
+    
+
+    carrito.appendChild(col4);
+    col4.appendChild(card);
+    card.appendChild(row);
+    row.appendChild(col6);
+    col6.appendChild(img);
+    row.appendChild(col62);
+    col62.appendChild(body);
+    body.appendChild(tt);
+    body.appendChild(desc);
+    body.appendChild(btn);
+    
+}
 
 const buscadorPrincipal = document.getElementById("buscador");
 
@@ -23,3 +135,38 @@ buscadorPrincipal.addEventListener("click", () => {
     window.location.replace("./tienda.html");
     
 })
+
+//cargo el carrito
+
+let carrito = localStorage.getItem("carrito");
+
+if (carrito != undefined && carrito.length > 0) {
+
+    carrito = JSON.parse(carrito);
+    let total = 0;
+    carrito.forEach(element => {
+        
+        makeCarritoItem(element);
+
+        const textTotal = document.getElementById("totalCarrito");
+
+        total = total + (element.price * element.cantidad);
+
+        textTotal.textContent = "Total: $" + total;
+
+    });
+}
+else {
+    const carrito = document.getElementById("carritoContainer");
+
+    const tt = document.createElement("h3");
+
+    tt.textContent = "No hay ningun producto en su carrito";
+
+    carrito.appendChild(tt);
+
+    const textTotal = document.getElementById("totalCarrito");
+
+    textTotal.textContent = "Total: $0"
+    
+}
